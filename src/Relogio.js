15d94ba9dca2fa50a4cb70392b9731eccc4dacbc
Fernando Dadalto <99692626+Fernando-a11y@users.noreply.github.com>
2024-04-30 10:49:14 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,12 +1,37 @@
+import React from 'react';
 
+class Relogio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDateTime: new Date().toLocaleString()
+    };
+  }
 
-function Relogio() {
-  return (
-    <>
-    <p>Meu Relógio</p>
-    </>
-    
-  );
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    this.setState({
+      currentDateTime: new Date().toLocaleString()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Data e Hora Atuais: {this.state.currentDateTime}</p>
+      </div>
+    );
+  }
 }
 
 export default Relogio;
