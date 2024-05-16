1b75f46e842fc472cb05359921565b5838ab2e32
Fernando Dadalto <99692626+Fernando-a11y@users.noreply.github.com>
2024-05-16 12:17:13 -0300

    Update index.js

--- src/Atividade03/Componentes/Gallery/index.js
+++ src/Atividade03/Componentes/Gallery/index.js
@@ -1,4 +1,11 @@
-import Profile from "../Profile";
+export function Profile(){
+return(
+    <img src= "https://i.imgur.com/QIrZWGIs.jpg"
+    alt="Alan L Hart"
+    />
+
+    );
+}
 
 export default function Gallery(){
     return(
