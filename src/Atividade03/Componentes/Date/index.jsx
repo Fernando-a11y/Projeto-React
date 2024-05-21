389b2f721f6c1b4c290e18c39fadad1f7f690cda
Fernando Dadalto <99692626+Fernando-a11y@users.noreply.github.com>
2024-05-21 19:54:11 -0300

    Update index.jsx

--- src/Atividade03/Componentes/Date/index.jsx
+++ src/Atividade03/Componentes/Date/index.jsx
@@ -1,22 +1,36 @@
 
+import React from 'react';
+
+function Avatar() {
+    const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+    const description = "Gregorio Y. Zara";
+    return (
+        <img
+            className="avatar"
+            src={avatar}
+            alt={description}
+        />
+    );
+}
+
 const today = new Date();
-function formatDate(date){
+function formatDate(date) {
     return new Intl.DateTimeFormat(
         'en-US',
-        {weekday: 'long'}
-        ).format(date);
-        
-    }
-    
-    export default function TodoList(){
-        const nome = 'Gregorio Y. Zara';
-        return(
-            <>
-            <h1>{nome}'s To Do List</h1>
-            <h1>{formatDate(today)}</h1>
-            </>
-        );
-    }
+        { weekday: 'long' }
+    ).format(date);
+}
+
+export default function TodoList() {
+    const name = 'Gregorio Y. Zara';
+    return (
+        <>
+            <h1>{name}'s To Do List</h1>
+            <h2>{formatDate(today)}</h2>
+            <Avatar />
+        </>
+    );
+}
 
 
 
