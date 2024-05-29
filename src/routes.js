cd7474277e390fa3ab9e7b71026629a5c4e8814f
Fernando Dadalto <99692626+Fernando-a11y@users.noreply.github.com>
2024-05-29 20:50:04 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Home from "./Home";
 import Atividade01 from "./Atividade01/atividade01";
 import Atividade02 from "./Atividade02/Atividade02";
 import Atividade03 from "./Atividade03/Atividade03";
-import Atividade03 from "./Atividade04/Atividade04";
+import Atividade04 from "./Atividade04/Atividade04";
 
 
 export default function routes() {
