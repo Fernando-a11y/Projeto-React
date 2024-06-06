bee538145c29ddcb08a16c5200ace928bdd94d18
Fernando Dadalto <99692626+Fernando-a11y@users.noreply.github.com>
2024-06-05 22:47:53 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Atividade01 from "./Atividade01/atividade01";
 import Atividade02 from "./Atividade02/Atividade02";
 import Atividade03 from "./Atividade03/Atividade03";
 import Atividade04 from "./Atividade04/Atividade04";
+import Atividade04 from "./Atividade05/Atividade05";
 
 
 export default function routes() {
@@ -15,6 +16,7 @@ export default function routes() {
                 <Route path="/Atividade02" element={<Atividade02/>}/>
                 <Route path="/Atividade03" element={<Atividade03/>}/>
                 <Route path="/Atividade04" element={<Atividade04/>}/>
+                <Route path="/Atividade05" element={<Atividade04/>}/>
                 
             </Routes>
         </BrowserRouter>
